4757f8082fab6855c19984158d20a756b130921c
mdshamoon <mohdshamoon841@gmail.com>
2018-08-19 16:28:22 +0530

    new interface

--- app/src/main/java/com/example/mdshamoon/examportal/faculty_login.java
+++ app/src/main/java/com/example/mdshamoon/examportal/faculty_login.java
@@ -1,13 +1,24 @@
 package com.example.mdshamoon.examportal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class faculty_login extends AppCompatActivity {
-
+Button fregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_login);
+        fregister=findViewById(R.id.btn_signup);
+        fregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(faculty_login.this,faculty_register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mdshamoon/examportal/homeactivity.java
+++ app/src/main/java/com/example/mdshamoon/examportal/homeactivity.java
@@ -7,17 +7,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class homeactivity extends AppCompatActivity {
-    Button flogin,fregister,slogin,sregister;
+    Button student,faculty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeactivity);
         getSupportActionBar().hide();
-        flogin=findViewById(R.id.LOGIN);
-        fregister=findViewById(R.id.register);
-        slogin=findViewById(R.id.LOGIN1);
-        sregister=findViewById(R.id.register1);
-        flogin.setOnClickListener(new View.OnClickListener() {
+       student=findViewById(R.id.student);
+       faculty=findViewById(R.id.faculty);
+        faculty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(homeactivity.this,faculty_login.class);
